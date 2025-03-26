5e3e38eaf3576ed78c49b5b2f7cdab2666eb5d87
john-c-dev <113213078+john-c-dev@users.noreply.github.com>
2025-03-26 14:20:33 -0400

    Async

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,7 +59,7 @@ public_users.get('/', async function(req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',async function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbnDetail = {}
   try {
     const isbn = req.params.isbn
@@ -75,7 +75,7 @@ public_users.get('/isbn/:isbn',async function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   try {
     const authorName = req.params.author
     const filteredBooks = {}
@@ -97,7 +97,7 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   try {
     const title = req.params.title
     const filteredBooks = {}
